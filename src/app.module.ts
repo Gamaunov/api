f385caabf7bc24ddc4681413e3ff25f4a8e79a9e
Denis Gamayunov <gamaunov1911@gmail.com>
2023-11-23 11:12:05 +0300

    fixed config

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,6 +2,7 @@ import * as dotenv from 'dotenv';
 import { Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
 import { ConfigModule } from '@nestjs/config';
+import * as process from 'process';
 
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
@@ -9,12 +10,10 @@ import { MainModule } from './modules/main.module';
 
 dotenv.config();
 
+const ROOT = process.env.MONGO_URI || 'mongodb://localhost:27017';
+
 @Module({
-  imports: [
-    ConfigModule.forRoot(),
-    MongooseModule.forRoot(process.env.MONGO_URI!),
-    MainModule,
-  ],
+  imports: [ConfigModule.forRoot(), MongooseModule.forRoot(ROOT), MainModule],
   controllers: [AppController],
   providers: [AppService],
 })
