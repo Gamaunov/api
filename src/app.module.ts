0f45424e3ac178db7db76c72fd32b77e5b23e09a
Denis Gamayunov <gamaunov1911@gmail.com>
2023-11-23 11:57:21 +0300

    fixed config

--- src/app.module.ts
+++ src/app.module.ts
@@ -10,10 +10,20 @@ import { MainModule } from './modules/main.module';
 
 dotenv.config();
 
-const ROOT = process.env.MONGO_URI || 'mongodb://localhost:27017';
+const isTest = process.env.NODE_ENV === 'test';
+
+const prodMongooseConfig =
+  process.env.MONGO_URI || 'mongodb://0.0.0.0:27017/production';
+
+const testMongooseConfig =
+  process.env.MONGO_TEST_URI || 'mongodb://0.0.0.0:27017/test';
 
 @Module({
-  imports: [ConfigModule.forRoot(), MongooseModule.forRoot(ROOT), MainModule],
+  imports: [
+    ConfigModule.forRoot(),
+    MongooseModule.forRoot(isTest ? testMongooseConfig : prodMongooseConfig),
+    MainModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
