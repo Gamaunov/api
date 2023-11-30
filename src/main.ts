ff0946d988385a2a80e6950187f3bd63002a0439
Denis Gamayunov <gamaunov1911@gmail.com>
2023-11-30 08:31:31 +0300

    removed ts

--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,7 @@ import { AppModule } from './app.module';
 import { appSettings } from './app.settings';
 
 const PORT = parseInt(process.env.PORT, 10) || 5000;
-async function bootstrap(): Promise<void> {
+async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   appSettings(app);
