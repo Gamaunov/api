3e4434e81333d05b94e64149ce14b065d8d730bf
Denis Gamayunov <gamaunov1911@gmail.com>
2023-12-07 15:04:15 +0300

    fixed blogInputDto

--- src/features/blogs/dto/blog-input.dto.ts
+++ src/features/blogs/dto/blog-input.dto.ts
@@ -1,13 +1,11 @@
-import { IsNotEmpty, IsUrl, Matches, MaxLength } from 'class-validator';
+import { IsNotEmpty, IsUrl, MaxLength } from 'class-validator';
 
 export class BlogInputDTO {
   @IsNotEmpty()
-  @Matches(/^[a-zA-Z]+$/)
   @MaxLength(15)
   name: string;
 
   @IsNotEmpty()
-  @Matches(/^[a-zA-Z]+$/)
   @MaxLength(500)
   description: string;
 
