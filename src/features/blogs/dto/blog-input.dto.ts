4496c3040ab154f61ff15ab2e86d182dd20b5381
Denis Gamayunov <gamaunov1911@gmail.com>
2023-12-07 15:29:30 +0300

    add custom decorator IsNotEmptyString, fixed PostInputDTO, BlogInputDTO

--- src/features/blogs/dto/blog-input.dto.ts
+++ src/features/blogs/dto/blog-input.dto.ts
@@ -1,11 +1,17 @@
-import { IsNotEmpty, IsUrl, MaxLength } from 'class-validator';
+import { IsNotEmpty, IsString, IsUrl, MaxLength } from 'class-validator';
+
+import { IsNotEmptyString } from '../../../shared/exceptions/decorators/is-not-empty-string.decorator';
 
 export class BlogInputDTO {
+  @IsString()
   @IsNotEmpty()
+  @IsNotEmptyString()
   @MaxLength(15)
   name: string;
 
+  @IsString()
   @IsNotEmpty()
+  @IsNotEmptyString()
   @MaxLength(500)
   description: string;
 
--- src/features/posts/dto/post-input.dto.ts
+++ src/features/posts/dto/post-input.dto.ts
@@ -1,15 +1,22 @@
 import { IsNotEmpty, IsOptional, IsString, MaxLength } from 'class-validator';
+
+import { IsNotEmptyString } from '../../../shared/exceptions/decorators/is-not-empty-string.decorator';
 export class PostInputDTO {
   @IsString()
   @IsNotEmpty()
+  @IsNotEmptyString()
   @MaxLength(30)
   title: string;
 
   @IsString()
+  @IsNotEmpty()
+  @IsNotEmptyString()
   @MaxLength(100)
   shortDescription: string;
 
+  @IsString()
   @IsNotEmpty()
+  @IsNotEmptyString()
   @MaxLength(1000)
   content: string;
 
